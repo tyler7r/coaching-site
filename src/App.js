6ece7589ce5ed1228790c80b88a5ce467d8eda15
Tyler Randall <tyler7r@gmail.com>
2023-08-28 18:19:05 -0400

    Set up skeleton files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './Home';
+import { Contact } from './Contact';
+import { Pricing } from './Pricing';
+import { Header } from './Header';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/pricing' element={<Pricing />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 

0e8ef7a2b24295fe3b8513e77c7fcd11bc7a32a9
Tyler Randall <tyler7r@gmail.com>
2023-08-29 13:43:05 -0400

    Create contact page for desktop view

--- src/Contact.js
+++ src/Contact.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import ContactImage from './images/contact-image.jpg'
+import EmailIcon from './images/email.svg'
+import PhoneIcon from './images/phone.svg'
+import './contact.css'
 
 export const Contact = (props) => {
     return (
-        <>
-        </>
+        <div id='contact-container'>
+            <div id='contact-title'>Contact</div>
+            <img src={ContactImage} alt='main' id='contact-image' />
+            <div id='contact-details'>
+            <div id='contact-message'>You're so close to upping your Ultimate game, book today!</div>
+                <div className='contact-detail-container'>
+                    <img src={EmailIcon} alt='email-icon' className='contact-detail-image' />
+                    <a href='mailto:tyler7r@gmail.com' className='contact-detail-info'>tyler7r@gmail.com</a>
+                </div>
+                <div className='contact-detail-container'>
+                    <img src={PhoneIcon} alt='phone-icon' className='contact-detail-image' />
+                    <a href='phone:4049847244' className='contact-detail-info'>404-984-7244</a>
+                </div>
+            </div>
+        </div>
     )
 }
--- src/Pricing.js
+++ src/Pricing.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PricingImage from './images/pricing-image.jpg'
+import './pricing.css'
 
 export const Pricing = (props) => {
+    const [groupOption, setGroupOption] = useState('2-3')
+
+    const optionSelect = (option) => {
+        if (option === groupOption) {
+            return <div className='option-select-btn selected' onClick={() => setGroupOption(option)}>{option}</div>
+        } else {
+            return <div className='option-select-btn' onClick={() => setGroupOption(option)}>{option}</div>
+        }
+    }
+
     return (
         <div id='pricing-container'>
-            
+            <div id='pricing-title'>Pricing</div>
+            <img src={PricingImage} alt='main' id='pricing-image' />
+            <div className='pricing-section-title' id='individual-pricing-title'>INDIVIDUAL</div>
+            <div className='pricing-section' id='individual-section'>
+                <div className='option-description'>1-Hour Session: <div className='option-price'>$80</div></div>
+                <div className='option-description'>90-Minute Session: <div className='option-price'>$100</div></div>
+                <div className='option-description'>30-Minute Film Review: <div className='option-price'>$50</div></div>
+            </div>
+            <div className='pricing-section-title' id='group-pricing-title'>GROUP</div>
+            <div className='pricing-section' id='group-section'>
+                <div id='option-select-container'>
+                    {optionSelect('2-3')}
+                    {optionSelect('4+')}
+                </div>
+                {groupOption === '2-3' &&
+                    <div className='pricing-info'>
+                        <div className='option-description'>1-Hour Session: <div className='option-price'>$60/person</div></div>
+                        <div className='option-description'>90-Minute Session: <div className='option-price'>$80/person</div></div>
+                    </div>
+                }
+                {groupOption === '4+' &&
+                    <div className='pricing-info'>
+                        <div className='option-description'>1-Hour Session: <div className='option-price'>$50/person</div></div>
+                        <div className='option-description'>90-Minute Session: <div className='option-price'>$70/person</div></div>
+                    </div>
+                }
+            </div>
         </div>
     )
 }
